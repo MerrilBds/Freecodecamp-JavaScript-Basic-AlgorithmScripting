5a9d85f8c0629c0c8c1a3492cac091545d05eec4
MerrilBds <Bdsmerril@gmail.com>
2023-07-09 15:16:28 +0200

    leçon 5 fin

---
+++ #05.LargestNumber-in-a-multiDim-array/large.js
@@ -0,0 +1,20 @@
+function largestOfFour(arr) {
+    let largestNumbers = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let largestNumber = arr[i][0];
+
+        for (let j = 1; j < arr[i].length; j++) {
+            if (arr[i][j] > largestNumber) {
+                largestNumber = arr[i][j];
+            }
+        }
+
+        largestNumbers.push(largestNumber);
+    }
+
+    return largestNumbers;
+}
+
+let large = largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+console.log(large);
